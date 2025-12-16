ccd0444aab9cf0a93341189e2bd3183a6d68d5b6
mehmetpektass <pektasmehmett1184@gmail.com>
2025-12-16 14:37:27 +0300

    initialize inngest dev server setup

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from agentic-rag-with-inngest-llamaindex!")
+import inngest
+import inngest.fast_api
+from inngest.experimental import ai
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import uuid
+import os
+import logging
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+inngest_client = inngest.Inngest(
+    app_id="rag_app",
+    logger=logging.getLogger("uvicorn"),
+    is_production=False,
+    serializer=inngest.PydanticSerializer()
+)
+
+@inngest_client.create_function(
+    fn_id="RAG: Inngest PDF",
+    trigger=inngest.TriggerEvent(event="rag/ingest_pdf"),
+)
+
+async def rag_ingest_pdf(ctx: inngest.Context):
+    return "Hello Guys"
+    
+app = FastAPI()
+
+inngest.fast_api.serve(app, inngest_client, [rag_ingest_pdf])    
